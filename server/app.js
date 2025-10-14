95f421a7abb0813e9cb26744f74bfaf1ccc84d6a
dianayukari <yukaridiana@gmail.com>
2025-10-14 22:07:35 +0200

    fix: update CORS origin to specify the correct GitHub Pages URL

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://dianayukari.github.io/'
+    'https://dianayukari.github.io/randominfo'
   ]
 }))
 
