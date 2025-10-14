2ec32dc3c0b48c91aa5029dc5a290ded91202fca
dianayukari <yukaridiana@gmail.com>
2025-10-14 21:27:09 +0200

    fix: update CORS origin to remove specific path from GitHub Pages URL

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use(cors({
     'http://127.0.0.1:5173',
     'http://localhost:4173',
     'http://127.0.0.1:4173',
-    'https://dianayukari.github.io/randominfo'
+    'https://dianayukari.github.io/'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
