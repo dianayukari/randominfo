7cf9e7f7a943c2703f541905a8bb161828128b68
dianayukari <yukaridiana@gmail.com>
2025-10-17 09:11:55 +0200

    Updates

---
+++ app/immutable/chunks/BRvj5Lgp.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1neefng?.base??"/randominfo",e=globalThis.__sveltekit_1neefng?.assets??s??"";export{e as a,s as b};
--- app/immutable/entry/start.GzNmsooi.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/D0IoRmqh.js";export{o as load_css,r as start};
