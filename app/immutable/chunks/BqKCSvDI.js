433d3cbe107155bed84aeb90c235c4d2df97db7a
dianayukari <yukaridiana@gmail.com>
2025-12-09 15:35:40 +0100

    Updates

---
+++ app/immutable/chunks/BqKCSvDI.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_12747bm?.base??"/randominfo",a=globalThis.__sveltekit_12747bm?.assets??s??"";export{a,s as b};
--- app/immutable/chunks/DIwL4uRD.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_jtbbdi?.base??"/randominfo",t=globalThis.__sveltekit_jtbbdi?.assets??s??"";export{t as a,s as b};
